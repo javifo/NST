543b2f6aacc48253800ceb749752a085e375ae9b
Javier Ferrer <javi.f.o@gmail.com>
2012-07-26 16:57:40 +0200

    Kernel: correct mkimage call

--- kernel/scripts/mkuboot.sh
+++ kernel/scripts/mkuboot.sh
@@ -9,9 +9,17 @@ MKIMAGE=$(type -path "${CROSS_COMPILE}mkimage")
 if [ -z "${MKIMAGE}" ]; then
 	MKIMAGE=$(type -path mkimage)
 	if [ -z "${MKIMAGE}" ]; then
+		# Add mkimage to path if not yet there. Note that kernel and u-boot are sibling directories in B&N directory structure
+	    export PATH=$PATH:$PWD/../u-boot/tools
+	    MKIMAGE=$(type -path mkimage)
+	    if [ -z "${MKIMAGE}" ]; then
 		# Doesn't exist
 		echo '"mkimage" command not found - U-Boot images will not be built' >&2
+		echo "Maybe you haven't built u-boot yet. Change to u-boot directory and build it" >&2
 		exit 0;
+		else
+		echo '"mkimage" not in path. Executable has been found and added to path. Continuing with the build...' >&2
+		fi
 	fi
 fi
 
